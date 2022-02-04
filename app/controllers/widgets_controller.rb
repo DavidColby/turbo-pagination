344fde97dbb05d0120a937b860c828b2c959bea2
David Colby <davidlcolby@gmail.com>
2022-02-04 15:45:38 -0600

    Prep for heroku deploy

--- app/controllers/widgets_controller.rb
+++ app/controllers/widgets_controller.rb
@@ -21,40 +21,14 @@ class WidgetsController < ApplicationController
 
   # POST /widgets or /widgets.json
   def create
-    @widget = Widget.new(widget_params)
-
-    respond_to do |format|
-      if @widget.save
-        format.html { redirect_to widget_url(@widget), notice: "Widget was successfully created." }
-        format.json { render :show, status: :created, location: @widget }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @widget.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # PATCH/PUT /widgets/1 or /widgets/1.json
   def update
-    respond_to do |format|
-      if @widget.update(widget_params)
-        format.html { redirect_to widget_url(@widget), notice: "Widget was successfully updated." }
-        format.json { render :show, status: :ok, location: @widget }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @widget.errors, status: :unprocessable_entity }
-      end
-    end
   end
 
   # DELETE /widgets/1 or /widgets/1.json
   def destroy
-    @widget.destroy
-
-    respond_to do |format|
-      format.html { redirect_to widgets_url, notice: "Widget was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
